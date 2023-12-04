853c3cd852c41ce1b5ea7d6173a83bfe86c4fbf3
Vo Phuoc <vonguyenhongphuoc2000@gmail.com>
2023-12-04 22:09:52 +0700

    Create Movie Model

--- app/src/main/java/com/devhp/firstcompose/screens/home/HomeScreen.kt
+++ app/src/main/java/com/devhp/firstcompose/screens/home/HomeScreen.kt
@@ -1,39 +1,26 @@
 package com.devhp.firstcompose.screens.home
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CornerSize
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.devhp.firstcompose.model.Movie
+import com.devhp.firstcompose.model.getMovies
 import com.devhp.firstcompose.navigation.MovieScreens
 import com.devhp.firstcompose.util.MyToast
+import com.devhp.firstcompose.widgets.MovieRow
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -56,16 +43,7 @@ fun HomeScreen(navController: NavController ){
 @Composable
 fun MainContent(
     navController: NavController,
-    movieList: List<String> = listOf(
-        "Avatar",
-        "300",
-        "Harry Potter",
-        "Happiness...",
-        "Cross the Line...",
-        "Be Happy...",
-        "Happy Feet...",
-        "Life"
-    )
+    movieList: List<Movie> = getMovies()
 ) {
     val context = LocalContext.current
     Column(modifier = Modifier.padding(12.dp)) {
@@ -80,35 +58,3 @@ fun MainContent(
     }
 }
 
-@Composable
-fun MovieRow(movie: String, onClick: (String) -> Unit) {
-    Card(
-        modifier = Modifier
-            .padding(4.dp)
-            .fillMaxWidth()
-            .height(130.dp)
-            .clickable {
-                onClick(movie)
-            },
-        shape = RoundedCornerShape(corner = CornerSize(16.dp)),
-        elevation = CardDefaults.cardElevation(6.dp)
-    ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Start,
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Surface(
-                modifier = Modifier
-                    .padding(12.dp)
-                    .size(100.dp),
-                shape = RectangleShape,
-                shadowElevation = 4.dp,
-            ) {
-                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "")
-
-            }
-            Text(text = movie)
-        }
-    }
-}

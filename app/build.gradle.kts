9809c2c1409a392d779cd93a6602907bbaa4739c
Vo Phuoc <vonguyenhongphuoc2000@gmail.com>
2023-12-05 21:09:12 +0700

    Adding Expand Capability on the Card to show More Info

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,9 +67,16 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // FFmpeg
     implementation("com.arthenica:ffmpeg-kit-full:6.0-2")
 
+    // Icons
     implementation("androidx.compose.material:material-icons-extended")
 
+    // Navigation
     implementation("androidx.navigation:navigation-compose:2.7.5")
+
+    // Coil
+    implementation("io.coil-kt:coil-compose:2.5.0")
 }
--- app/src/main/java/com/devhp/firstcompose/widgets/widgets.kt
+++ app/src/main/java/com/devhp/firstcompose/widgets/widgets.kt
@@ -1,28 +1,49 @@
 package com.devhp.firstcompose.widgets
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.devhp.firstcompose.R
 import com.devhp.firstcompose.model.Movie
 import com.devhp.firstcompose.model.getMovies
 
@@ -30,11 +51,16 @@ import com.devhp.firstcompose.model.getMovies
 @Preview
 @Composable
 fun MovieRow(movie: Movie = getMovies()[0], onClick: (String) -> Unit = {}) {
+
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
     Card(
         modifier = Modifier
             .padding(4.dp)
             .fillMaxWidth()
-            .height(130.dp)
+//            .height(130.dp)
             .clickable {
                 onClick(movie.id)
             },
@@ -53,10 +79,54 @@ fun MovieRow(movie: Movie = getMovies()[0], onClick: (String) -> Unit = {}) {
                 shape = RectangleShape,
                 shadowElevation = 4.dp,
             ) {
-                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "")
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data("https://i.ytimg.com/vi/DQAHg4e9-wk/sddefault.jpg?v=64529d55")
+                        .crossfade(true).build(),
+                    contentDescription = "Movie Poster",
+                    placeholder = painterResource(id = R.drawable.user_profile),
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.clip(CircleShape)
+                )
+//                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "")
 
             }
-            Text(text = movie.title)
+            Column(modifier = Modifier.padding(4.dp)) {
+                Text(text = movie.title, style = MaterialTheme.typography.titleLarge)
+                Text(text = "Director: ${movie.title}", style = MaterialTheme.typography.bodyMedium)
+                Text(text = "Released: ${movie.year}", style = MaterialTheme.typography.bodyMedium)
+
+                AnimatedVisibility(visible = expanded) {
+                    Column {
+                        Text(buildAnnotatedString {
+                            withStyle(style = SpanStyle(color = Color.DarkGray, fontSize = 13.sp)) {
+                                append("Plot: ")
+                            }
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.DarkGray,
+                                    fontSize = 13.sp,
+                                    fontWeight = FontWeight.Bold
+                                )
+                            ) {
+                                append(movie.plot)
+                            }
+                        })
+                    }
+                }
+
+                Icon(
+                    imageVector = if (expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                    contentDescription = "Down Arrow",
+                    modifier = Modifier
+                        .size(25.dp)
+                        .clickable {
+                            expanded = !expanded
+                        }, tint = Color.DarkGray
+                )
+            }
         }
+
+
     }
 }

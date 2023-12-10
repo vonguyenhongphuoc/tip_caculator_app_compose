be0c7a99014188b9e438f872fa18252e46b93555
Vo Phuoc <vonguyenhongphuoc2000@gmail.com>
2023-12-10 18:40:23 +0700

    Fishing up the repository class

--- app/src/main/java/com/devhp/firstcompose/repository/QuestionRepository.kt
+++ app/src/main/java/com/devhp/firstcompose/repository/QuestionRepository.kt
@@ -1,10 +1,23 @@
 package com.devhp.firstcompose.repository
 
+import android.util.Log
 import com.devhp.firstcompose.data.DataOrException
 import com.devhp.firstcompose.model.QuestionItem
 import com.devhp.firstcompose.network.QuestionApi
 import javax.inject.Inject
 
 class QuestionRepository @Inject constructor(private val api: QuestionApi) {
-    private val listOfQuestions = DataOrException<ArrayList<QuestionItem>, Boolean,Exception>()
+    private val dataOrException = DataOrException<ArrayList<QuestionItem>, Boolean, Exception>()
+
+    suspend fun getAllQuestions(): DataOrException<ArrayList<QuestionItem>, Boolean, Exception> {
+        try {
+            dataOrException.loading = true
+            dataOrException.data = api.getAllQuestion()
+            if (dataOrException.data.toString().isNotEmpty()) dataOrException.loading = false
+        } catch (e: Exception) {
+            dataOrException.e = e
+            Log.d("Exc", "getAllQuestions: ${dataOrException.e?.localizedMessage}")
+        }
+        return dataOrException
+    }
 }

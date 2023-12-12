0b197a8382b442ee9e3f362565a718881be9777f
Vo Phuoc <vonguyenhongphuoc2000@gmail.com>
2023-12-12 22:06:21 +0700

    Build Splash Screen UI

--- app/src/main/java/com/devhp/firstcompose/screen/WeatherSplashScreen.kt
+++ app/src/main/java/com/devhp/firstcompose/screen/WeatherSplashScreen.kt
@@ -1,10 +1,51 @@
 package com.devhp.firstcompose.screen
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.devhp.firstcompose.R
 
+@Preview
 @Composable
-fun WeatherSplashScreen(navController: NavController){
-    Text(text = "Splash Screen")
+fun WeatherSplashScreen(navController: NavController = rememberNavController()) {
+    Surface(
+        modifier = Modifier
+            .padding(15.dp)
+            .size(330.dp),
+        shape = CircleShape,
+        color = Color.White,
+        border = BorderStroke(width = 2.dp, color = Color.LightGray)
+    ) {
+        Column(
+            modifier = Modifier.padding(1.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.sun),
+                contentDescription = "Image Sun",
+                contentScale = ContentScale.Fit, modifier = Modifier.size(95.dp)
+            )
+            Text(text = "Find the Sun?", style = MaterialTheme.typography.titleSmall, color = Color.LightGray)
+
+        }
+
+    }
 }

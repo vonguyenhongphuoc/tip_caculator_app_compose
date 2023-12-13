327fb88c852a4f954315d53144f89b2aff824bda
Vo Phuoc <vonguyenhongphuoc2000@gmail.com>
2023-12-13 22:31:20 +0700

    Setting up the ViewModel and Retrieving Json Payload

--- app/src/main/java/com/devhp/firstcompose/screen/main/MainScreen.kt
+++ app/src/main/java/com/devhp/firstcompose/screen/main/MainScreen.kt
@@ -3,11 +3,8 @@ package com.devhp.firstcompose.screen.main
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.produceState
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import com.devhp.firstcompose.data.DataOrException
-import com.devhp.firstcompose.model.Weather
 
 @Composable
 fun MainScreen(navController: NavController, mainViewModel: MainViewModel = hiltViewModel()) {
@@ -16,12 +13,7 @@ fun MainScreen(navController: NavController, mainViewModel: MainViewModel = hilt
 
 @Composable
 fun ShowData(mainViewModel: MainViewModel) {
-    val weatherData = produceState<DataOrException<Weather, Boolean, Exception>>(
-        initialValue = DataOrException(loading = true)
-    ) {
-        value = mainViewModel.getWeatherData(city = "Seattle")
-    }.value
-
+    val weatherData = mainViewModel.data.value
     if (weatherData.loading == true) {
         CircularProgressIndicator()
     } else if (weatherData.data != null) {
--- app/src/main/java/com/devhp/firstcompose/screen/main/MainViewModel.kt
+++ app/src/main/java/com/devhp/firstcompose/screen/main/MainViewModel.kt
@@ -11,13 +11,37 @@ import com.devhp.firstcompose.model.Weather
 import com.devhp.firstcompose.repository.WeatherRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(private val repository: WeatherRepository) : ViewModel() {
- suspend fun getWeatherData(city: String) : DataOrException<Weather, Boolean, Exception>{
-     return repository.getWeather(cityQuery = city)
- }
+
+    val data: MutableState<DataOrException<Weather, Boolean, Exception>> = mutableStateOf(
+        DataOrException(null, true, null)
+    )
+
+    init {
+          loadData()
+    }
+
+    private fun loadData() {
+        viewModelScope.launch {
+            coroutineScope {
+                data.value.loading = true
+                withContext(Dispatchers.IO){
+                    data.value = repository.getWeather("Seattle")
+                }
+                if(data.value.data != null) data.value.loading = false
+            }
+        }
+
+    }
+
+    suspend fun getWeatherData(city: String): DataOrException<Weather, Boolean, Exception> {
+        return repository.getWeather(cityQuery = city)
+    }
 
 }

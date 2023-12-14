bd388a43615b3ab8a781c28b420d8278260bb017
Vo Phuoc <vonguyenhongphuoc2000@gmail.com>
2023-12-14 22:27:14 +0700

    Create Main Screen Part 1

--- app/src/main/java/com/devhp/firstcompose/screen/main/MainScreen.kt
+++ app/src/main/java/com/devhp/firstcompose/screen/main/MainScreen.kt
@@ -1,17 +1,31 @@
 package com.devhp.firstcompose.screen.main
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.compose.rememberAsyncImagePainter
 import com.devhp.firstcompose.data.DataOrException
 import com.devhp.firstcompose.model.Weather
 import com.devhp.firstcompose.widget.WeatherAppBar
@@ -54,6 +68,63 @@ fun MainScaffold(weather: Weather, navController: NavController) {
 }
 
 @Composable
-fun MainContent(weather: Weather) {
+fun MainContent(data: Weather) {
+    val imageUrl = "https://openweathermap.org/img/wn/${data.list!![0].weather!![0].icon}.png"
+    Column(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Nov 29",
+            style = TextStyle(
+                fontSize = 12.sp,
+                fontWeight = FontWeight.SemiBold,
+                letterSpacing = 0.4.sp
+            ),
+            color = MaterialTheme.colorScheme.secondary,
+            modifier = Modifier.padding(6.dp)
+        )
 
+        Surface(
+            modifier = Modifier
+                .padding(4.dp)
+                .size(200.dp), shape = CircleShape, color = Color(0XFFFFC400)
+        ) {
+
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                WeatherStateImage(imageUrl = imageUrl)
+                Text(
+                    text = "56",
+                    style = TextStyle(
+                        fontSize = 34.sp,
+                        fontWeight = FontWeight.ExtraBold,
+                        letterSpacing = 0.25.sp
+                    )
+                )
+                Text(
+                    text = "Snow",
+                    style = TextStyle(
+                        fontStyle = FontStyle.Italic
+                    )
+                )
+
+            }
+        }
+
+    }
+}
+
+@Composable
+fun WeatherStateImage(imageUrl: String) {
+    Image(
+        painter = rememberAsyncImagePainter(imageUrl),
+        contentDescription = "",
+        modifier = Modifier.size(80.dp)
+    )
 }
--- app/src/main/java/com/devhp/firstcompose/util/Constants.kt
+++ app/src/main/java/com/devhp/firstcompose/util/Constants.kt
@@ -1,6 +1,8 @@
 package com.devhp.firstcompose.util
 
 object Constants {
+    //https://api.openweathermap.org/data/2.5/forecast/daily?q=lisbon&appid=ed60fcfbd110ee65c7150605ea8aceea&units=imperial
+    //https://openweathermap.org/img/wn/10d.png
     const val BASE_URL = "https://api.openweathermap.org/"
     const val API_KEY = "ed60fcfbd110ee65c7150605ea8aceea"
 }

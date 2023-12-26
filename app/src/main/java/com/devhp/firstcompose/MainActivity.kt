af8ead17e45e4e5c11d7119a5b65782c6f9c8e4d
Vo Phuoc <vonguyenhongphuoc2000@gmail.com>
2023-12-26 21:59:36 +0700

    Clean code for FCM

--- app/src/main/java/com/devhp/firstcompose/MainActivity.kt
+++ app/src/main/java/com/devhp/firstcompose/MainActivity.kt
@@ -3,15 +3,9 @@ package com.devhp.firstcompose
 
 
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -20,80 +14,19 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.core.content.ContextCompat
 import com.devhp.firstcompose.navigation.ReaderNavigation
 import com.devhp.firstcompose.ui.theme.FirstComposeTheme
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.messaging.FirebaseMessaging
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    // Declare the launcher at the top of your Activity/Fragment:
-    private val requestPermissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestPermission(),
-    ) { isGranted: Boolean ->
-        if (isGranted) {
-            // FCM SDK (and your app) can post notifications.
-        } else {
-            // TODO: Inform user that that your app will not show notifications.
-        }
-    }
-
-    private fun askNotificationPermission() {
-        // This is only necessary for API level >= 33 (TIRAMISU)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) ==
-                PackageManager.PERMISSION_GRANTED
-            ) {
-                // FCM SDK (and your app) can post notifications.
-                Log.d("MyTag", "Notification Permission is Granted")
-            } else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
-                // TODO: display an educational UI explaining to the user the features that will be enabled
-                //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
-                //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
-                //       If the user selects "No thanks," allow the user to continue without notifications.
-            } else {
-                // Directly ask for the permission
-                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
-            }
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        askNotificationPermission()
-//frxzafRNSTOAUg9cTAdtBn:APA91bGp3_H6RRR6YLhH7A1UO5BW1T8IlwRDjiCjGkFcv33G5RvRW2LE08Nw-3BMbjI3qiEH4rJa4V6Kv7w3JtVCplNkEpHAlI9LVKFC8PzeZzMdWZqe5Ui2gtF9DjlAO8uhkqLK6lgU
-        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
-            if (!task.isSuccessful) {
-                Log.w("MyTag", "Fetching FCM registration token failed", task.exception)
-                return@OnCompleteListener
-            }
-
-            // Get new FCM registration token
-            val token = task.result
-
-            // Log and toast
-            val msg = "FCM Token: $token"
-            Log.d("MyTag", msg)
-            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-        })
-
         setContent {
             FirstComposeTheme {
-//                val db = FirebaseFirestore.getInstance()
-//                val user: MutableMap<String, Any> = HashMap()
-//                user["firstName"] = "Jeo"
-//                user["lastName"] = "James"
-//                db.collection("users").add(user).addOnSuccessListener {
-//                    Log.d("MyTag", "Create user success: ${it.id}")
-//                }.addOnFailureListener {
-//                    Log.d("MyTag", "Create user failure: ${it.localizedMessage}")
-//                }
                 ReaderApp()
-
             }
         }
     }
